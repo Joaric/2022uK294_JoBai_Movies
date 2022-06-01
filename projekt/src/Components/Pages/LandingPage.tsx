aa61764a995c92b755a3b4e5c6e3ca6ec918f39d
joao.baixinho <joao.baixinho@noseryoung.ch>
2022-06-01 16:46:45 +0200

    Made Atomic design folders, started with navbar components from material UI

---
+++ projekt/src/Components/Pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import Navbar from '../Organisms/Navbar';
+
+export class LandingPage extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Navbar />
+          </div>
+    )
+  }
+}
+
+export default LandingPage;
